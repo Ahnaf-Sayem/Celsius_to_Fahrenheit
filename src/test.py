25365fa4020a26d086e976430c8d0cc65c0d2ebd
Ahnaf-Sayem <ahnafsayeem.ctg@gmail.com>
2026-03-13 12:36:13 -0700

    All the updates have been made but still a lot of things to go

---
+++ src/test.py
@@ -0,0 +1,6 @@
+
+import train
+import numpy
+from train import model
+user_input=(float(input('Enter your temperature in celsius:')))
+print(model.predict(numpy.array(user_input)))
---
+++ src/train.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import numpy as np
+
+
+c = np.array([-40, -10,  0,  8, 15, 22,  38],  dtype=float)
+f = np.array([-40,  14, 32, 46, 59, 72, 100],  dtype=float)
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Dense(units=1, input_shape=[1])
+])
+
+model.compile(loss='mean_squared_error',
+              optimizer=tf.keras.optimizers.Adam(0.1), metrics=['mean_squared_error'])
+
+history = model.fit(c, f, epochs=500, verbose=False)
+print("Finished training the model")
