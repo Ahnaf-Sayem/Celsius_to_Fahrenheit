de75c13b59e2790792620678a1a1a884b4cc45b4
Ahnaf-Sayem <ahnafsayeem.ctg@gmail.com>
2026-03-13 12:44:15 -0700

    model.predict

--- src/test.py
+++ src/test.py
@@ -2,5 +2,5 @@
 import train
 import numpy
 from train import model
-user_input=(float(input('Enter your temperature in celsius:')))
-print(model.predict(numpy.array(user_input)))
+user_input=float(input('Enter your temperature in celsius:'))
+print(model.predict(numpy.array([user_input])))
